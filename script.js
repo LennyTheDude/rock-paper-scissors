20ed287e9ac673caa6b72c0c23230b98bbda4b80
LennyTheDude <lennyfromrussia@gmail.com>
2020-11-15 23:42:46 +0300

    event listeners for btns via delegation; cleaned code

--- script.js
+++ script.js
@@ -22,42 +22,36 @@ function round (playerSelection) { // play a round
         if ((playerSelection == "Rock" && computerSelection == ("Scissors"))||
         ((playerSelection == "Paper" && computerSelection == ("Rock")))||
         ((playerSelection == "Scissors" && computerSelection == ("Paper")))) {
-            result = "You win this round, " + playerSelection + " beats " + computerSelection + "!";
+            result = "You won this round, " + playerSelection + " beats " + computerSelection + "!";
             plyr = plyr + 1;
             score[1].innerHTML = plyr;
         } else {
-            result = "You lose this round, " + computerSelection + " beats " + playerSelection + "!";
+            result = "You lost this round, " + computerSelection + " beats " + playerSelection + "!";
             comp = comp + 1;
             score[3].innerHTML = comp;
         }
     }
-    choices[4].innerHTML = result;
+    
     if (plyr == 5||comp == 5) {
         if (plyr == 5) {
-            choices[4].innerHTML = "You won the game!";
+            result = "You won the game!";
         } else if (comp == 5) {
-            choices[4].innerHTML = "You lost the game!";
+            result = "You lost the game!";
         }
-     
     }
-    // console.log(result);
-    return result;
+    choices[4].innerHTML = result;
 }
 
 let plyr = 0; // player's score
 let comp = 0; // computer's score
 let score = document.getElementsByClassName('score')[0].children; // where we display the scores
-// console.log(score);
-let selections = document.getElementsByClassName('selection'); // buttons that select player's move
-// console.log(selections);
+let selections = document.getElementById('buttons'); // the div around buttons
 let choices = document.getElementsByClassName('choices')[0].children; // where we display what choices were made
-// console.log(choices);
 
-for (const [key, value] of Object.entries(selections)) { // assigning events to buttons
-    value.addEventListener("click", function () {
+selections.addEventListener("click", event => { // assigning events to buttons
+    if (event.target.className === "selection") {
         if (plyr < 5 && comp < 5) {
-            round(this.getAttribute('data-play'));
+            round(event.target.innerHTML);
         }
-    });
-}
-
+    }
+});
