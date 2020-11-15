2d3c7ef50d783e9231571d7b747ed5ef5a169fb7
LennyTheDude <lennyfromrussia@gmail.com>
2020-11-15 22:54:40 +0300

    game now behaves as per specs

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function computerPlay () {
+function computerPlay () { // randomize computer's selection
     let number = Math.floor(Math.random() * 99);
     let result;
     if (number % 3 == 0) {
@@ -11,72 +11,53 @@ function computerPlay () {
     return result;
 }
 
-/*function capitalize (str) {
-    let start = str.substring(0, 1);
-    start = start.toUpperCase();
-    str = str.toLowerCase();
-    str = start + str.slice(1);
-    return str;
-}*/
-
-function round (playerSelection) {
+function round (playerSelection) { // play a round
     let computerSelection = computerPlay();
-    let result;
+    choices[0].innerHTML = playerSelection;
+    choices[2].innerHTML = computerSelection;
+    let result = '';
     if (playerSelection == computerSelection) {
-        result = "This round is a draw, " + playerSelection + " and " + computerSelection + " are the same!";
+        result = "Draw, both chose " + playerSelection + "!";
     } else {
         if ((playerSelection == "Rock" && computerSelection == ("Scissors"))||
         ((playerSelection == "Paper" && computerSelection == ("Rock")))||
         ((playerSelection == "Scissors" && computerSelection == ("Paper")))) {
             result = "You win this round, " + playerSelection + " beats " + computerSelection + "!";
             plyr = plyr + 1;
-            score[0].children[0].innerHTML = plyr;
+            score[1].innerHTML = plyr;
         } else {
             result = "You lose this round, " + computerSelection + " beats " + playerSelection + "!";
             comp = comp + 1;
-            score[0].children[2].innerHTML = comp;
+            score[3].innerHTML = comp;
         }
     }
+    choices[4].innerHTML = result;
+    if (plyr == 5||comp == 5) {
+        if (plyr == 5) {
+            choices[4].innerHTML = "You won the game!";
+        } else if (comp == 5) {
+            choices[4].innerHTML = "You lost the game!";
+        }
+     
+    }
+    // console.log(result);
     return result;
 }
 
-let plyr = 0;
-let comp = 0;
-let score = document.getElementsByClassName('score');
-//console.log(score[0].children[2]);
-let choices = document.getElementsByClassName('play');
-console.log(choices);
+let plyr = 0; // player's score
+let comp = 0; // computer's score
+let score = document.getElementsByClassName('score')[0].children; // where we display the scores
+// console.log(score);
+let selections = document.getElementsByClassName('selection'); // buttons that select player's move
+// console.log(selections);
+let choices = document.getElementsByClassName('choices')[0].children; // where we display what choices were made
+// console.log(choices);
 
-
-for (const [key, value] of Object.entries(choices)) {
+for (const [key, value] of Object.entries(selections)) { // assigning events to buttons
     value.addEventListener("click", function () {
-        let choice = value.getAttribute('data-play');
-        //console.log(choice);
-        round(choice);
+        if (plyr < 5 && comp < 5) {
+            round(this.getAttribute('data-play'));
+        }
     });
 }
 
-
-
-  
-  
-/*   
-function game () {
-    let playerSelection, computerSelection;
-     for (let i = 1; i < 6; i++) {
-        playerSelection = prompt("Rock, Paper, or Scissors?");
-        playerSelection = capitalize(playerSelection);
-        computerSelection = computerPlay();
-        let res = round(playerSelection, computerSelection);
-        console.log(res);
-        console.log("Score: You - " + plyr + ", Computer - " + comp);
-    }
-    if (plyr > comp) {
-        console.log("You won the game! Final score: You - " + plyr + ", Computer - " + comp);
-    } else if (plyr < comp) {
-        console.log("You lost the game! Final score: You - " + plyr + ", Computer - " + comp);
-    } else {
-        console.log("It's a draw! Final score: You - " + plyr + ", Computer - " + comp);
-    }
-}
-*/
